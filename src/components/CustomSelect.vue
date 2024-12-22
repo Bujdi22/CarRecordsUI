af9301e6d98b6a6c0f3bc03d94b68a090ca03e36
Balazs Bujdoso <balazs.bujdoso@hrduo.com>
2024-12-22 20:48:51 +0000

    model selector wip

---
+++ src/components/CustomSelect.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import VueSelect from "vue3-select-component";
+
+defineProps(['modelValue', 'options', 'isItem'])
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div :class="{'select-item-wrapper': isItem}">
+      <p class="select-title" v-if="$slots.default">
+        <slot>
+        </slot>
+      </p>
+    <VueSelect
+        :modelValue="modelValue"
+        @update:modelValue="(v) => $emit('update:modelValue', v)"
+        :options="options"
+        placeholder="Select an option"
+    />
+  </div>
+</template>
