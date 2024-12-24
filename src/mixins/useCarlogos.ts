593cb2625cfec805cd98316ab7eba7a09f2279e2
Balazs Bujdoso <balazs.bujdoso@hrduo.com>
2024-12-24 17:00:03 +0000

    model selector

--- src/mixins/useCarlogos.ts
+++ src/mixins/useCarlogos.ts
@@ -1,15 +1,8 @@
 import { defineComponent } from 'vue';
-import carmakers from "@/assets/carmakers.json";
-
-interface CarLogo {
-    name: string,
-    path: string,
-}
 export default defineComponent({
     data() {
       return {
           logoImports: {},
-          carmakers: carmakers.brands as CarLogo[],
       }
     },
     created() {
@@ -17,11 +10,11 @@ export default defineComponent({
     },
     methods: {
         async getLogo(brandName: string) {
-            const brand = this.carmakers.find((b) => b.name === brandName);
-            if (!brand) return null;
+
+            const fileName = brandName.toLowerCase().split(' ').join('') + '.png';
 
             // Dynamically load the logo if it's available
-            const importLogo = this.logoImports[`/src/assets/carlogos/${brand.path}`];
+            const importLogo = this.logoImports[`/src/assets/carlogos/${fileName}`];
             if (importLogo) {
                 const module = await importLogo();
                 return module.default;
