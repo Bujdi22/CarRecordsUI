6bdb3a4b31498a0ab9885f76ffdd911e7b9ca2a2
bujdi <bu.buba2@gmail.com>
2025-06-12 19:23:48 +0100

    chores

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,11 @@ import {defineConfig, loadEnv} from 'vite'
 // https://vitejs.dev/config/
 
 export default defineConfig(({ mode }) => {
+	
 
   const env = loadEnv(mode, process.cwd()); // Only VITE_*
+
+  console.log({env, mode});
 
   return {
     plugins: [
